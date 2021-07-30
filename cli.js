f566f78f9a59bd6fcb6c9de00a84972af38f122f
kuncc <kuncc@foxmail.com>
2021-07-30 18:43:06 +0800

    update

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const program = require('commander')
 const api = require('./index')
-const pkg = require('package.json')
+const pkg = require('./package.json')
 
 program
   .version(pkg.version)
